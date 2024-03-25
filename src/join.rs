cdb3f73fff0665073c5c9c60879ee5c89c0b870f
Amar Sood <pkg@tekacs.com>
2024-03-25 08:25:35 -0700

    Make JoinHandle... joinable.

--- src/join.rs
+++ src/join.rs
@@ -3,6 +3,18 @@ pub enum JoinHandle<T> {
     RemoteHandle(Option<futures_util::future::RemoteHandle<T>>),
 }
 
+impl<T> JoinHandle<T> {
+    pub async fn join(&mut self) -> T
+    where
+        T: 'static,
+    {
+        match self {
+            Self::Tokio(handle) => handle.await.unwrap(),
+            Self::RemoteHandle(handle) => handle.take().unwrap().await,
+        }
+    }
+}
+
 impl<T> Drop for JoinHandle<T> {
     /// To match Tokio behavior and make it easier to handle throwaway tasks,
     /// if a JoinHandle is dropped without the inner RemoteHandle being taken,
