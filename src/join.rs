d99fd4d1a7ba88c4b4b2a63d47e29a7f9b47409b
Amar Sood (tekacs) <pkg@tekacs.com>
2025-12-31 14:22:42 -0500

    Fix JoinHandle cfg gating
    - Gate Tokio/wasm JoinHandle variants by features
    - Add placeholder variant for no-runtime builds

--- src/join.rs
+++ src/join.rs
@@ -1,6 +1,10 @@
 pub enum JoinHandle<T> {
+    #[cfg(feature = "tokio")]
     Tokio(tokio::task::JoinHandle<T>),
+    #[cfg(feature = "wasm")]
     RemoteHandle(Option<futures_util::future::RemoteHandle<T>>),
+    #[doc(hidden)]
+    _Phantom(core::marker::PhantomData<T>),
 }
 
 impl<T> JoinHandle<T> {
@@ -9,8 +13,13 @@ impl<T> JoinHandle<T> {
         T: 'static,
     {
         match self {
+            #[cfg(feature = "tokio")]
             Self::Tokio(handle) => handle.await.unwrap(),
+            #[cfg(feature = "wasm")]
             Self::RemoteHandle(handle) => handle.take().unwrap().await,
+            Self::_Phantom(_) => panic!(
+                "No runtime is enabled. Enable the `tokio` or `wasm` feature to use a runtime."
+            ),
         }
     }
 }
@@ -21,12 +30,15 @@ impl<T> Drop for JoinHandle<T> {
     /// we simply forget it so that it's able to continue to completion.
     fn drop(&mut self) {
         match self {
+            #[cfg(feature = "wasm")]
             Self::RemoteHandle(handle) => {
                 if let Some(handle) = handle.take() {
                     handle.forget();
                 }
             }
+            #[cfg(feature = "tokio")]
             Self::Tokio(_) => {}
+            Self::_Phantom(_) => {}
         }
     }
 }
